709c61f73e865aeab41acea299b3a204d62face1
Nico Tejera <tejera.nicolas@gmail.com>
2021-09-09 13:28:13 -0400

    update

--- build/src/practice.js
+++ build/src/practice.js
@@ -3,14 +3,20 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.practice6 = exports.practice5 = exports.practice4 = exports.practice3 = exports.practice2 = exports.practice1 = undefined;
+
+var _nodeFetch = require('node-fetch');
+
+var _nodeFetch2 = _interopRequireDefault(_nodeFetch);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 /**
  * Write a function that returns a Promise that
  * resolves to 'E-39'
  */
 var practice1 = exports.practice1 = function practice1() {
-  return new Promise(function (resolve, reject) {
-    resolve('E-39');
-  });
+  // TODO
 };
 
 /**
@@ -18,9 +24,48 @@ var practice1 = exports.practice1 = function practice1() {
  * waits 3 seconds and then resolves to 'E-39
  */
 var practice2 = exports.practice2 = function practice2() {
-  return new Promise(function (resolve, reject) {
-    setTimeout(function () {
-      resolve('E-39');
-    }, 2000);
-  });
+  // TODO
+};
+
+/**
+ * Write a function that returns an array of 3 promises
+ * that each resolve to 'E-39-1', 'E-39-2', 'E-39-3' respectively
+ */
+var practice3 = exports.practice3 = function practice3() {
+  // TODO
+};
+
+/**
+ * Write a function that receives an argument 'n', and returns an array of
+ * N promises that each resolve to 'E-39-${N}' respectively
+ */
+var practice4 = exports.practice4 = function practice4(n) {
+  // TODO
+};
+
+/**
+ * Write a function that receives a single character and returns a Promise that:
+ * if the input is undefined or length different than 1, reject immediately with 'error
+ * if the character is a number, resolves after 1 seecond to 'number'
+ * if the character is a vowel, resolves after 2 seconds to 'vowel'
+ * else resolves after 3 seconds to 'consonant'
+ */
+var practice5 = exports.practice5 = function practice5(c) {
+  // TODO
+};
+
+/**
+ * Write a function that receives a string category and:
+ *
+ * - if the category = 'explicit' reject to 'explicit joke'
+ * - if the category is not provided, reject to 'no category'
+ * - else retrieves a random Chuck Norris joke and:
+ *  - if the returned joke includes the category 'explicit', reject to 'explicit joke'
+ *  - else returns a Promise that resolves to the joke, plus a
+ *    header = 'safe for E-39'
+ *
+ * API: https://api.chucknorris.io/jokes/random
+ */
+var practice6 = exports.practice6 = function practice6(category) {
+  // TODO
 };
--- src/index.js
+++ src/index.js
@@ -1,2 +1,76 @@
 var l = console.log;
 
+// const old = 'hello ' + (1 + 2) + ' world';
+// l(old);
+
+// const t2 = `hello ${1 + 2} world`;
+
+// const multiline = `
+//  hi 1
+//  hi 2
+//  hi 3
+//  `;
+
+// //  const uselessFunction = () => {
+// //    return 'something';
+// //  }
+
+// //  const res = uselessFunction`hello world`;
+// //  l(res);
+
+// //  const merger = (pieces, ...values) => {
+// //   const merged = [];
+
+// //   for (let i = 0; i < pieces.length; i++) {
+// //     merged.push(pieces[i]);
+// //     if (values[i]) {
+// //       merged.push(values[i]);
+// //     }
+// //   }
+
+// //   return merged.join('');
+// //  }
+
+// //  const name = 'Nico';
+// //  const other = ['Vue', 'Angular'].sort(() => Math.random() - 0.5)[0];
+// //  const res = merger`my name is ${name}, React is better than ${other}`;
+// //  l(res);
+
+// //  const italicize = (pieces, ...values) => {
+// //   const merged = [];
+
+// //   for (let i = 0; i < pieces.length; i++) {
+// //     merged.push(pieces[i]);
+// //     if (values[i]) {
+// //       merged.push(`<i>${values[i]}</i>`);
+// //     }
+// //   }
+
+// //   return merged.join('');
+// // }
+
+// const request = n => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log(`${n} - inside promise - completed`);
+//       resolve('Im done');
+//     }, 5000);
+//   });
+// };
+
+// const work = n => {
+//   console.log(`${n} - Before request...`, new Date());
+
+//   console.log(`${n} - Making request...`, new Date());
+//   request(n).then(r => {
+//     console.log(`${n} - in work`, r);
+//   });
+
+//   console.log(`${n} - After request...`, new Date());
+
+//   setTimeout(() => {
+//     work(n + 1);
+//   }, 1000);
+// };
+
+// work(0);
